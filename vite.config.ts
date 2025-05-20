d25e1064641911a50c9bfc1cb0e7205ea98d9c81
H1p4zdev <138879774+H1p4zdev@users.noreply.github.com>
2025-05-20 16:19:25 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/h1p4zdev.github.io/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
